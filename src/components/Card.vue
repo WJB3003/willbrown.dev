c492b1ab8dddaecec2db572aec2bbcdd7420a5b3
William <wbrown@kaleido-health.com>
2019-12-28 20:17:07 -0500

    commit

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,24 +2,26 @@
   <div class="card">
     <div class="card-top">
       <h2></></h2>
-      <div style="display: flex">
+      <div class="icon">
         <div v-for="icon in this.project.icons">
           <a :href="icon.link" target="_blank">
-            <img :src="icon.img" height="20px" style="padding-left: 10px">
+            <img style="padding-left: 10px" :src="icon.img" height="20px">
           </a>
         </div>
       </div>
     </div>
     <div class="card-bottom">
-      <div class="project-desc">
+      <div style="min-height: 0px">
         <h2>{{this.project.title}}</h2>
         <p>{{this.project.summary}}</p>
       </div>
+      <div style="height: 0px">
+        <h4 style="margin:0; padding-bottom:7px">Technologies:</h4>
+        <ul class="project-tech">
+          <li class="project-skill" v-for="tech in this.project.technologies">{{tech}}</li>
+        </ul>
+      </div>
     </div>
-    <h4 style="margin: 0">Technologies:</h4>
-    <ul class="project-tech">
-      <li class="project-skill" style="white-space: nowrap" v-for="tech in this.project.technologies">{{tech}}</li>
-    </ul>
   </div>
 </template>
 
@@ -33,5 +35,53 @@
 
 <style>
 
+h2 {
+  font-size: 14px;
+}
+
+.icon {
+  display: flex;
+  align-items: center;
+}
+
+.card{
+  min-width: 220px;
+  min-height: 350px;
+  max-width: 220px;
+  max-height: 350px;
+  background-color: #42B883;
+  border-radius: 10px;
+  padding: 30px;
+  margin: 40px 25px 40px;
+}
+
+.card:hover{
+  position: relative;
+  top: -5px;
+}
+
+.card-top{
+  display: flex;
+  justify-content: space-between;
+}
+
+.card-bottom{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.project-tech{
+  display: grid;
+  grid-template-columns: 105px 105px;
+  grid-column-gap: 10px;
+  width: 100%;
+  padding: 0;
+}
+
+.project-skill{
+  list-style-type: none;
+  white-space: nowrap;
+}
 
 </style>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -13,7 +13,7 @@
       <div class="panel home">
         <h1 id="title">{{name}}</h1>
         <div id="divider"></div>
-        <h2 id="occupation">{{title}}</h2>
+        <h2 style="font-size: 80px">{{title}}</h2>
       </div>
 
       <div class="panel" id="about">
@@ -30,14 +30,16 @@
       </div>
     </div>
 
-    <div class="panel" id="work">
+    <div id="work">
       <div id="work-title">WORK</div>
       <div class="hl-work"></div>
+      <div>
       <ul class="cards">
         <li v-for="project in projects">
           <Card :project='project'/>
         </li>
       </ul>
+      </div>
     </div>
 
     <div class="panel" id="contact">
@@ -81,293 +83,232 @@
 </script>
 
 <style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.3;
-}
-
-.layout{
-  font-family: cambria;
-}
-
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 12.5px 0px;
-  background-color: #35495E;
-  opacity: .9;
-}
-
-#home-icon{
-  padding-left: 12.5px;
-  max-height: 37px;
-}
-
-.nav{
-  display: flex;
-  font-size: 25px;
-}
-
-.nav-item{
-  margin-right: 25px;
-  color: white;
-}
-
-.panel{
-  min-height: 100vh;
-}
-
-.home{
-  min-height: calc(100vh - 62px);
-  color: #35495E;
-  margin-left: 5%;
-}
-
-#about{
-  background-color: #35495E;
-  color: white;
-  opacity: .9;
-}
-
-.hl-about{
-  margin-left: 5%;
-  height: 2px;
-  width: 70%;
-  background-color: white;
-}
-
-.about-title{
-  font-size: 75px;
-  padding: 20px 0 0 20px;
-  margin-left: 5%;
-}
-
-.about-summary{
-  font-size: 35px;
-  padding: 0 150px 0 150px;
-  text-align: center;
-  align-items: center;
-  justify-content: center;
-  display: flex;
-}
-
-.about-container{
-  display: flex;
-  min-height: 73vh;
-  height: calc(100vh - 132px);
-}
-
-.skills-container {
-  max-width: 270px;
-  display: flex;
-  flex-basis: 150%;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
-}
-
-.skills{
-  display: block;
-}
-
-.skill{
-  font-size: 80px;
-  width: 80px;
-  height: 80px;
-  margin-right: 10px;
-}
-
-.skill:hover{
-  color: #42b883;
-}
-
-.skill-row{
-  display: flex;
-  margin-top: 10px;
-}
-
-#work{
-  min-height: 100vh;
-  background-color: #0A192F;
-  color: white;
-}
-
-.hl-work{
-  margin-left: 15%;
-  height: 2px;
-  width: 70%;
-  background-color: #35495E;
-}
-
-#contact{
-  background-color: black;
-  color: white;
-  display: flex;
-  min-height: 100vh;
-}
-
-.contact-desc{
-  font-size: 55px;
-  min-width: 75%;
-  text-align: center;
-  justify-content: center;
-  display: flex;
-  align-items: center;
-}
-
-.contact-item-container{
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-width: 24%;
-}
-
-.contact-items{
-  display: inline-block;
-}
-
-.contact-item{
-  font-size: 20px;
-  min-height: 35px;
-  display: flex;
-  margin-bottom: 10px;
-}
-
-.contact-item-text{
-  padding: 2px;
-}
-
-.vl{
-  margin-top: 7vh;
-  height: 86vh;
-  width: 2px;
-  background-color: white;
-}
-
-h2{
-  margin: 0;
-}
-
-h1{
-  margin: 0%;
-}
-
-p{
-  margin: 0%;
-}
-
-.half-face-background{
-  background-image: url(https://i.imgur.com/PZaRMLD.jpg);
-  height: 200vh;
-  background-size: cover;
-  background-position-x: right;
-}
-
-#title{
-  font-size: 85px;
-  padding-top: 11%;
-}
-
-#occupation{
-  font-size: 80px;
-}
-
-#work-title{
-  font-size: 75px;
-  text-align: center;
-  padding-top: 30px;
-}
-
-#divider{
-  background-color: #42B883;
-  height: 10px;
-  width: 72%;
-}
-
-.icons{
-  padding-right: 15px;
-}
-
-.card{
-  min-width: 220px;
-  min-height: 350px;
-  max-width: 220px;
-  max-height: 350px;
-  background-color: #42B883;
-  border-radius: 10px;
-  padding: 30px;
-  margin: 80px 25px 80px;
-}
-
-.card:hover{
-  position: relative;
-  top: -5px;
-}
-
-.cards{
-  margin: 0;
-  list-style-type: none;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: calc(100vh - 142px);
-}
-
-.project-skill{
-  list-style-type: none;
-  padding-right: 10px;
-}
-
-.project-title{
-  font-size: 25px;
-  margin-bottom: 5px;
-}
-
-.project-tech{
-  display: flex;
-  flex-wrap: wrap;
-  padding: 0;
-}
-
-.card-top{
-  display: flex;
-  justify-content: space-between;
-}
-
-.card-bottom{
-  margin-bottom: 100px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-}
-
-.link{
-  display: flex;
-  padding-top: 5px;
-}
-
-.project-link{
-  padding-left: 10px;
-}
-
-.project-icon{
-  font-weight: bolder;
-  font-size: 20px;
-}
-
-a{
-  color: white;
-  text-decoration: none;
-  padding: 0px;
-  margin: 0px;
-}
-
-@media only screen and (max-width: 500px) {
-  html{
-    display: none;
+  body {
+    font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
+    margin:0;
+    padding:0;
+    line-height: 1.3;
   }
-}
+
+  .layout {
+    font-family: cambria;
+  }
+
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 12.5px 0px;
+    background-color: #35495E;
+    opacity: .9;
+  }
+
+  #home-icon {
+    padding-left: 12.5px;
+    max-height: 37px;
+  }
+
+  .nav {
+    display: flex;
+    font-size: 25px;
+  }
+
+  .nav-item {
+    margin-right: 25px;
+    color: white;
+  }
+
+  .panel {
+    min-height: 100vh;
+  }
+
+  .home {
+    min-height: calc(100vh - 62px);
+    color: #35495E;
+    margin-left: 5%;
+  }
+
+  #about {
+    background-color: #35495E;
+    color: white;
+    opacity: .9;
+  }
+
+  .hl-about {
+    margin-left: 5%;
+    height: 2px;
+    width: 70%;
+    background-color: white;
+  }
+
+  .about-title {
+    font-size: 75px;
+    padding: 20px 0 0 20px;
+    margin-left: 5%;
+  }
+
+  .about-summary {
+    font-size: 35px;
+    padding: 0 150px 0 150px;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    display: flex;
+  }
+
+  .about-container {
+    display: flex;
+    min-height: 73vh;
+    height: calc(100vh - 132px);
+  }
+
+  .skills-container {
+    max-width: 270px;
+    display: flex;
+    flex-basis: 150%;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .skills {
+    display: block;
+  }
+
+  .skill {
+    font-size: 80px;
+    width: 80px;
+    height: 80px;
+    margin-right: 10px;
+  }
+
+  .skill:hover {
+    color: #42b883;
+  }
+
+  #work {
+    min-height: 100vh;
+    background-color: #0A192F;
+    color: white;
+  }
+
+  .hl-work {
+    margin-left: 15%;
+    height: 2px;
+    width: 70%;
+    background-color: #35495E;
+  }
+
+  #contact {
+    background-color: black;
+    color: white;
+    display: flex;
+    min-height: 100vh;
+  }
+
+  .contact-desc {
+    font-size: 55px;
+    min-width: 75%;
+    text-align: center;
+    justify-content: center;
+    display: flex;
+    align-items: center;
+  }
+
+  .contact-item-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-width: 24%;
+  }
+
+  .contact-items {
+    display: inline-block;
+  }
+
+  .contact-item {
+    font-size: 20px;
+    min-height: 35px;
+    display: flex;
+    margin-bottom: 10px;
+  }
+
+  .contact-item-text {
+    padding: 2px;
+  }
+
+  .vl {
+    margin-top: 7vh;
+    height: 86vh;
+    width: 2px;
+    background-color: white;
+  }
+
+  h2 {
+    margin: 0;
+  }
+
+  h1 {
+    margin: 0%;
+  }
+
+  p {
+    margin: 0%;
+  }
+
+  .half-face-background {
+    background-image: url(https://i.imgur.com/PZaRMLD.jpg);
+    height: 200vh;
+    background-size: cover;
+    background-position-x: right;
+  }
+
+  #title {
+    font-size: 85px;
+    padding-top: 11%;
+  }
+
+  #work-title {
+    font-size: 75px;
+    text-align: center;
+    padding-top: 30px;
+  }
+
+  #divider {
+    background-color: #42B883;
+    height: 10px;
+    width: 72%;
+  }
+
+  .icons {
+    padding-right: 15px;
+  }
+
+  .cards {
+    margin: 0;
+    list-style-type: none;
+    display: flex;
+    flex-wrap: wrap;
+    flex-basis: 150px;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .link {
+    display: flex;
+    padding-top: 5px;
+  }
+
+  a {
+    color: white;
+    text-decoration: none;
+    padding: 0px;
+    margin: 0px;
+  }
+
+  @media only screen and (max-width: 500px) {
+    html{
+      display: none;
+    }
+  }
 </style>
